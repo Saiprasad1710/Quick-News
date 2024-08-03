5068f5c0c00b2ab9084aa153547d11d7d1ac6613
SaiPrasad <kirsambresaiprasad@gmail.com>
2024-08-03 14:24:04 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom'
 
     <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
-        <Link className="navbar-brand" to="/home">NewsToday</Link>
+        <Link className="navbar-brand" to="/home">QuickNews</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
@@ -35,10 +35,7 @@ import { Link } from 'react-router-dom'
            
             
         </ul>
-        <form className="d-flex">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        
         </div>
     </div>
     </nav>
